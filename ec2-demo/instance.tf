f82a78f3926e5102ecf9d2089041f7d84c549fc1
learndevops-083 <113036991+learndevops-083@users.noreply.github.com>
2022-10-19 20:05:36 +0100

    Update instance.tf

--- ec2-demo/instance.tf
+++ ec2-demo/instance.tf
@@ -1,7 +1,7 @@
 #Terraform Resource Block
 resource "aws_instance" "ec2-demo1" {
     ami = data.aws_ami.amzlinux2.id
-    instance_type = var.aws_instance_type
+    instance_type = var.instance_type
     user_data = file("${path.module}/app1-install.sh")
     key_name = var.instance_keypair
     vpc_security_group_ids = [ aws_security_group.ssh.id,aws_security_group.web.id ]
